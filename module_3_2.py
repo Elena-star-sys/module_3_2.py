03528992f8de8a061d2594474162dce76dbd8047
Elena-star-sys <elenashulgaelena@gmail.com>
2024-08-15 21:15:57 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,23 +1,19 @@
 def send_email(message, recipient, sender="university.help@gmail.com"):
+    if "@" not in recipient or not recipient.endswith(('.com', '.ru', '.net')):
 
-    if "@" not in recipient or not recipient.endswith(('.com', '.ru', 'net')):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
 
-        print("Невозможно отправить письмо с адреса {} на адрес {}")
+    if "@" not in sender or not sender.endswith(('.com', '.ru', '.net')):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
 
     elif sender == recipient:
 
         print("Невозможно отправить письмо самому себе")
 
     else:
-         print("Письмо отправлено успешно")
-
-         if "@" not in sender or not sender.endswith(('.com', '.ru', 'net')):
-            print("Невозможно отправить письмо с адреса {} на адрес {}")
-
-
+        print(f"Письмо отправлено успешно с адреса: {sender} на адрес: {recipient}")
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
-
